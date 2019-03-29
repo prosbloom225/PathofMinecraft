8fc36b7275279f07d78d0b391735a65c3de5ed79
tk93053 <michael.osiecki@kohls.com>
2019-03-29 08:13:59 -0500

    calling process only on server, breaks clicking out, but better than processing twice

--- src/main/java/com/prosbloom/pom/events/PomEvents.java
+++ src/main/java/com/prosbloom/pom/events/PomEvents.java
@@ -11,6 +11,7 @@ import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber(value =
         { Side.SERVER, Side.CLIENT })
@@ -22,21 +23,27 @@ public class PomEvents {
         System.out.println("Picked up: " + event.getItem());
     }
 
+    //@SideOnly(Side.CLIENT)
     @SubscribeEvent
     public static void AnvilRepairEvent(AnvilRepairEvent event) {
         ItemStack rightStack = event.getIngredientInput();
         ItemStack outputStack = event.getItemResult();
         ItemStack leftStack = event.getItemInput();
-        // TODO - event seems to be triggering twice.  really need to clean this up.
 
-        if (rightStack.getItem() instanceof ICurrency) {
+        // TODO - ok so if you shift click a ModSword out of anvil, with a ModSword in hand, it breaks, fix it later
+        if (rightStack.getItem() instanceof ICurrency){
+            ItemStack newStack = leftStack.copy();
+            // only apply currency on server
+            if (event.getEntity().getEntityWorld().isRemote == false)
+                newStack = ((ICurrency) rightStack.getItem()).process(newStack);
+
+            // TODO - item in hand isnt refreshed until a new container event triggers, item doesnt appear updated when mousing out of anvil
             // clicking item out of output slot leaves it in the anvil
             if (event.getEntityPlayer().inventory.getItemStack().getItem() instanceof ModSword)
-                event.getEntityPlayer().inventory.setItemStack(((ICurrency)rightStack.getItem()).process(leftStack.copy()));
+                event.getEntityPlayer().inventory.setItemStack(newStack.copy());
             else
                 // shift click clears the anvil
-                event.getEntityPlayer().inventory.addItemStackToInventory(((ICurrency)rightStack.getItem()).process(leftStack.copy()));
-
+                event.getEntityPlayer().inventory.addItemStackToInventory(newStack);
             // remove the(all) dummy item(s)
             event.getEntityPlayer().inventory.mainInventory.stream()
                     .filter(stack->stack.getItem() instanceof ModSword && NbtHelper.isDummy(stack))
@@ -49,7 +56,7 @@ public class PomEvents {
     {
         ItemStack rightItem = event.getRight();
         ItemStack leftItem = event.getLeft();
-
+        // TODO - should check if the currency can apply here.  ie full on prefixes, dont let them exalt
         if (leftItem.getItem() instanceof ModSword && rightItem.getItem() instanceof ICurrency) {
             // we need to set a valid cost and output item to give the player something in output slot
             event.setCost(1);
